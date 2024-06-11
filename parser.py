7dad51a166dcd6ff0a8f0e8b836c7a9cc8c62540
Anudeep Tubati <anudeep-mac@Anudeeps-MBP.lan>
2024-06-11 16:43:43 -0400

    added GtP parser

---
+++ parser.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+from biothings import config
+
+logging = config.logger
+
+
+def load_interactions(data_folder: str):
+    infile = os.path.join(data_folder, "approved_drug_detailed_interactions.csv")
+    assert os.path.exists(infile)
+
+    # pk: Ligand ID,Target ID,Target Ligand ID,Target Species
+    # inner join of primary_targets_csv[pk] and detailed_csv[pk] is primary_targets_csv[pk]
+    with open(infile, "r") as data_fd:
+        reader = csv.DictReader(data_fd)
+        next(reader)  # skip first two rows
+        next(reader)
+        idx = 0
+
+        for row in reader:
+            row["_id"] = str(idx)
+            idx += 1
+            yield row
