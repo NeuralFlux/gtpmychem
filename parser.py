2790f488895c32783827f9eba5c8c1bbf580cf99
Anudeep Tubati <anudeep-mac@Anudeeps-MBP.lan>
2024-06-13 17:40:26 -0400

    added multi source data, ligands

--- parser.py
+++ parser.py
@@ -7,17 +7,28 @@ logging = config.logger
 
 
 def load_interactions(data_folder: str):
-    infile = os.path.join(data_folder, "approved_drug_detailed_interactions.csv")
-    assert os.path.exists(infile)
-
     # pk: Ligand ID,Target ID,Target Ligand ID,Target Species
     # inner join of primary_targets_csv[pk] and detailed_csv[pk] is primary_targets_csv[pk]
-    with open(infile, "r") as data_fd:
+    interactions_file = os.path.join(data_folder, "approved_drug_detailed_interactions.csv")
+    assert os.path.exists(interactions_file)
+
+    with open(interactions_file, "r") as data_fd:
         next(data_fd)  # skip metadata row
         reader = csv.DictReader(data_fd)
-        idx = 0
 
         for row in reader:
-            row["_id"] = str(idx)
-            idx += 1
+            row["_id"] = row["Ligand ID"]
+            yield row
+
+
+def load_ligands(data_folder: str):
+    ligands_file = os.path.join(data_folder, "ligands.csv")
+    assert os.path.exists(ligands_file)
+
+    with open(ligands_file, "r") as data_fd:
+        next(data_fd)  # skip metadata row
+        reader = csv.DictReader(data_fd)
+
+        for row in reader:
+            row["_id"] = row["Ligand ID"]
             yield row
