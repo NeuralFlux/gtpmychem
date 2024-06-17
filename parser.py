d30614c0f5895f0cd6d720386dd3e1ca2b9de435
Anudeep Tubati <anudeep-mac@Anudeeps-MBP.lan>
2024-06-17 16:08:24 -0400

    revamped ligands collection to include interactions as a field

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
-import csv
 import os
 
+import numpy as np
+import pandas as pd
 from biothings import config
 from biothings.utils.dataload import dict_convert, dict_sweep
 
@@ -8,42 +9,45 @@ logging = config.logger
 
 VAL_MAP = {"yes": True, "no": False}
 process_key = lambda key: key.replace(" ", "_").lower()
-process_val = lambda val: VAL_MAP[val] if val in VAL_MAP.keys() else val
+process_val = lambda val: VAL_MAP[val] if type(val) == str and val in VAL_MAP.keys() else val
 
 
-def load_interactions(data_folder: str):
-    # pk: Ligand ID,Target ID,Target Ligand ID,Target Species
-    # inner join of primary_targets_csv[pk] and detailed_csv[pk] is primary_targets_csv[pk]
-    interactions_file = os.path.join(data_folder, "approved_drug_detailed_interactions.csv")
-    assert os.path.exists(interactions_file)
-
-    with open(interactions_file, "r") as data_fd:
-        next(data_fd)  # skip metadata row
-        reader = csv.DictReader(data_fd)
-
-        for row in reader:
-            # remove keys with empty string OR list of empty strings
-            row = dict_sweep(row, vals=[""], remove_invalid_list=True)
-            row = dict_convert(row, keyfn=process_key)
-            row = dict_convert(row, valuefn=process_val)
-
-            row["_id"] = row["ligand_id"]
-            yield row
+def preprocess_data(d: dict):
+    d = dict_sweep(d, vals=["", np.nan], remove_invalid_list=True)
+    d = dict_convert(d, keyfn=process_key)
+    d = dict_convert(d, valuefn=process_val)
+    return d
 
 
 def load_ligands(data_folder: str):
+    # pk: Ligand ID,Target ID,Target Ligand ID,Target Species
+    # inner join of primary_targets_csv[pk] and detailed_csv[pk] is primary_targets_csv[pk]
+    interactions_file = os.path.join(data_folder, "approved_drug_detailed_interactions.csv")
     ligands_file = os.path.join(data_folder, "ligands.csv")
-    assert os.path.exists(ligands_file)
+    assert os.path.exists(interactions_file) and os.path.exists(ligands_file)
 
-    with open(ligands_file, "r") as data_fd:
-        next(data_fd)  # skip metadata row
-        reader = csv.DictReader(data_fd)
+    ligands_df = (
+        pd.read_csv(ligands_file, skiprows=1, dtype=object)
+        .rename({"Ligand ID": "_id"}, axis=1)
+        .set_index("_id")
+    )
+    interactions_df = pd.read_csv(interactions_file, skiprows=1, dtype=object).rename(
+        {"Ligand": "Name"}, axis=1
+    )
 
-        for row in reader:
-            # remove keys with empty string OR list of empty strings
-            row = dict_sweep(row, vals=[""], remove_invalid_list=True)
-            row = dict_convert(row, keyfn=process_key)
-            row = dict_convert(row, valuefn=process_val)
+    # drop redundant common columns from interactions
+    cols_to_drop = ligands_df.columns.intersection(interactions_df.columns)
+    interactions = interactions_df.drop(cols_to_drop, axis=1).to_dict(orient="records")
+    ligands = ligands_df.to_dict(orient="index")
+    assert type(list(ligands.keys())[0]) == str
 
-            row["_id"] = row["ligand_id"]
-            yield row
+    for row in interactions:
+        ligand_id = str(row["Ligand ID"])
+        row.pop("Ligand ID")  # redundant since present in ligands
+
+        # NOTE: we assume ligand IDs in interactions will be found in ligands
+        ligands[ligand_id].setdefault("interactions", []).append(preprocess_data(row))
+
+    for k, ligand in ligands.items():
+        ligand["_id"] = k
+        yield preprocess_data(ligand)
