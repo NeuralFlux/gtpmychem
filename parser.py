24aba38ada0b0c659dc470b35883d22c1143b94b
Anudeep Tubati <anudeep-mac@Anudeeps-MBP.lan>
2024-06-18 11:59:41 -0400

    refactored code into utils and added more prepro

--- parser.py
+++ parser.py
@@ -1,22 +1,11 @@
 import os
 
-import numpy as np
 import pandas as pd
 from biothings import config
-from biothings.utils.dataload import dict_convert, dict_sweep
+
+import utils as ligand_utils
 
 logging = config.logger
-
-VAL_MAP = {"yes": True, "no": False}
-process_key = lambda key: key.replace(" ", "_").lower()
-process_val = lambda val: VAL_MAP[val] if type(val) == str and val in VAL_MAP.keys() else val
-
-
-def preprocess_data(d: dict):
-    d = dict_sweep(d, vals=["", np.nan], remove_invalid_list=True)
-    d = dict_convert(d, keyfn=process_key)
-    d = dict_convert(d, valuefn=process_val)
-    return d
 
 
 def load_ligands(data_folder: str):
@@ -26,17 +15,17 @@ def load_ligands(data_folder: str):
     ligands_file = os.path.join(data_folder, "ligands.csv")
     assert os.path.exists(interactions_file) and os.path.exists(ligands_file)
 
-    ligands_df = (
+    ligands = (
         pd.read_csv(ligands_file, skiprows=1, dtype=object)
         .rename({"Ligand ID": "_id"}, axis=1)
         .set_index("_id")
+        .to_dict(orient="index")
     )
-    interactions_df = pd.read_csv(interactions_file, skiprows=1, dtype=object)
-
-    # drop redundant common columns from interactions
-    cols_to_drop = ligands_df.columns.intersection(interactions_df.columns)
-    interactions = interactions_df.drop(cols_to_drop, axis=1).to_dict(orient="records")
-    ligands = ligands_df.to_dict(orient="index")
+    interactions = (
+        pd.read_csv(interactions_file, skiprows=1, dtype=object)
+        .rename(ligand_utils.intrs_rename_dict, axis=1)
+        .to_dict(orient="records")
+    )
     assert type(list(ligands.keys())[0]) == str
 
     for row in interactions:
@@ -48,26 +37,8 @@ def load_ligands(data_folder: str):
             ligands[ligand_id]["CAS Number"] = row["CAS Number"]
             ligands[ligand_id]["Clinical Use Comment"] = row["Clinical Use Comment"]
 
-        row["Name"] = row["Target"]
-        if isinstance(row["Target Species"], str):
-            row["Target Species"] = row["Target Species"].lower()
-
-        # redundant since present in ligands
-        cols_to_drop = [
-            "Ligand ID",
-            "CAS Number",
-            "Clinical Use Comment",
-            "Ligand Synonyms",
-            "Target",
-            "Ligand",
-        ]
-        for col in cols_to_drop:
-            row.pop(col)
-
-        ligands[ligand_id]["interaction_targets"].append(preprocess_data(row))
+        ligands[ligand_id]["interaction_targets"].append(ligand_utils.preprocess_intrs(row))
 
     for k, ligand in ligands.items():
         ligand["_id"] = k
-        if isinstance(ligand["Synonyms"], str):
-            ligand["Synonyms"] = ligand["Synonyms"].split("|")
-        yield preprocess_data(ligand)
+        yield ligand_utils.preprocess_ligands(ligand)
